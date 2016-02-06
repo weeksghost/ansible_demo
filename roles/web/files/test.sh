1bd1ae9f87fce901c4b792232e56abe8a59eea28
Erik Marty <erikomarty@gmail.com>
2016-02-06 16:15:30 -0500

    Nearly have it

--- roles/web/files/test.sh
+++ roles/web/files/test.sh
@@ -1,5 +1,5 @@
 export DISPLAY=:99
 
-cd pytest && python setup.py install && cd ..
+cd pytest && sudo python setup.py install && cd ..
 
 py.test --driver=chrome --baseurl='http://reddit.com'  pytest/tests.py
